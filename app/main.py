9a648a10490e0f466badc4506fab69d4c1b4b6d1
shaun2099 <shaun2099@gmail.com>
2022-07-02 18:40:31 +0800

    fix heroku deployment

--- app/main.py
+++ app/main.py
@@ -7,16 +7,16 @@ from . import models, schemas, utils
 from .database import Base, engine, get_db
 from sqlalchemy.orm import Session
 from .routers import post, user, auth
-from .config import settings
+# from .config import settings
 
-print(settings)
-models.Base.metadata.create_all(bind=engine)
+# print(settings)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
+# app.include_router(post.router)
+# app.include_router(user.router)
+# app.include_router(auth.router)
 
 @app.get('/')
 async def root():
